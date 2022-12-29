c169b08c1571770f8c203c7be28b62aa12a9e7ac
OfekBinder <ofrkbinder@gmail.com>
2022-12-29 11:32:32 +0200

    client

---
+++ client.h
@@ -0,0 +1,15 @@
+//
+// Created by User on 29/12/2022.
+//
+
+#ifndef EX3_BAR_OFEK_AP_AA_CLIENT_H
+#define EX3_BAR_OFEK_AP_AA_CLIENT_H
+
+
+class client {
+
+
+};
+
+
+#endif //EX3_BAR_OFEK_AP_AA_CLIENT_H
