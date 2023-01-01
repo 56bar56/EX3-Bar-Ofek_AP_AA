0d99cee42a41f1de89825bbaef502c9213f3f91e
OfekBinder <ofrkbinder@gmail.com>
2023-01-01 23:10:08 +0200

    h files

--- FileClass.h
+++ FileClass.h
@@ -5,12 +5,18 @@
 #ifndef EX2_AP_B_O_FILECLASS_H
 #define EX2_AP_B_O_FILECLASS_H
 
+#include "FileClass.h"
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cstdlib>
+#include "DistanceList.cpp"
 #include <iostream>
 #include <fstream>
 #include <string>
 
-
-class FileClass{};
-
-
+void merge(string array[], int const left, int const mid,int const right);
+void mergeSort(string array[], int const begin, int const end);
+string FoundTheName(string names[]);
+string FormerMainRunner(int kPlaces, string distanceKind, vector<float> vec, string fileRead);
 #endif //EX2_AP_B_O_FILECLASS_H
